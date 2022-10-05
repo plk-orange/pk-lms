70948fdefdd2caab62df1a4f1e467bd8c3f6fc46
Paul Kamradt <plkamradt@gmail.com>
2022-10-04 22:35:36 -0700

    swapping components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { useEffect, useState } from 'react'
 import { useRoutes } from 'react-router-dom'
 import { TablePK } from './components'
 import NavBar from './components/navbar'
+import NotStaying from './NotStaying'
 import routies from './pages'
+import { questions } from './utils/db/staticdb'
 
 function App() {
     const routes = useRoutes(routies)
 
     return (
         <div className='container mx-auto'>
-            <NavBar />
-
-            {routes}
+            {/* <NavBar />
+<
+            {routes} */}
+            <NotStaying list={questions} />
         </div>
     )
 }
